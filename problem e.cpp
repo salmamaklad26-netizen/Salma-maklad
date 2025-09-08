698d5c8cd518b973a19ece8f6a303e38ea18979b
salmamaklad26-netizen <salmamaklad26@gmail.com>
2025-09-08 20:16:46 +0300

    Add files via upload

---
+++ problem e.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;  
+
+    int maxNum = -1; 
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        if (x > maxNum) {
+            maxNum = x;
+        }
+    }
+
+    cout << maxNum << endl;
+    return 0;
+}
